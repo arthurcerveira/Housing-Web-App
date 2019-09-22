cdf52d6a6ae5a436d84d3c760edbf921df7ed960
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2019-09-22 01:07:04 -0300

    Implementa classes em javascript para trabalhar integração com html

--- script.js
+++ script.js
@@ -6,15 +6,66 @@ class Account {
     this.description = description;
     this.role = role;
   }
+
+  toString() {
+    str = this.name + "\n" + this.description + "\n";
+  }
 }
 
 class DataBase {
   constructor() {
-    var accounts = [];
+    this.accounts = [];
+    this.userInterface = new UserInterface();
   }
 
-  createAccount(email, password, name, description, role) {
-    account = new Account(email, password, name, description, role);
+  createAccount() {
+    var account = this.userInterface.getAccountInfo();
     this.accounts.push(account);
   }
+
+  displayAccounts() {
+    this.userInterface.accountUl.innerHTML = "";
+
+    this.accounts.forEach(account => {
+      this.userInterface.accountToLi(account);
+    });
+  }
 }
+
+class UserInterface {
+  constructor() {
+    this.accountUl = document.querySelector(".accounts");
+  }
+
+  accountToLi(account) {
+    var accountLi = document.createElement("li");
+    accountLi.textContent = account.name;
+    this.accountUl.appendChild(accountLi);
+  }
+
+  getAccountInfo() {
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    var name = document.getElementById("name");
+    var description = document.getElementById("description");
+    var role = document.getElementById("role");
+
+    var account = new Account(
+      email.value,
+      password.value,
+      name.value,
+      description.value,
+      role.value
+    );
+
+    email.value = "";
+    password.value = "";
+    name.value = "";
+    description.value = "";
+    role.value = "";
+
+    return account;
+  }
+}
+
+dataBase = new DataBase();
