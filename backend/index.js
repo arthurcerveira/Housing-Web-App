36a43d47a2dc5e76949cdabf50fc1f9715e945ce
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2019-09-26 11:59:59 -0300

    Adiciona arquivos de backend e gitignore. Implementa classe em index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,29 @@
-const Joi = require("joi");
+// const Joi = require("joi");
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use(express.json());
+
+const accounts = [];
+
+class Account {
+  constructor(email, password, name, description, role) {
+    this.email = email;
+    this.password = password;
+    this.name = name;
+    this.description = description;
+    this.role = role;
+  }
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../frontend/index.html"));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Listening on port " + port.toString() + "...");
+});
+
+console.log(__dirname);
