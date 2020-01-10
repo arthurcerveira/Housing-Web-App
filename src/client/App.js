920a58af7837580cdd62853ff20cdcc5517c15f3
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2020-01-10 12:16:09 -0200

    Refatora codigo para utilizar simple-react-full-stack  template

---
+++ src/client/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./app.css";
+
+export default class App extends Component {
+  componentDidMount() {}
+
+  render() {
+    return <h1>Housing Web App</h1>;
+  }
+}
--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-require("dotenv/config");
+require("dotenv").config();
 
 // Middleware
 const app = express();
@@ -25,5 +25,5 @@ mongoose.connect(
   () => console.log("Connected to DB")
 );
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port);
