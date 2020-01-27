de7193250edc57d508c894144f2534b9e6181ccb
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2020-01-27 11:42:38 -0300

    Adiciona settings do vscode a gitignore

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,28 +7,28 @@ API = 'http://localhost:8080/api/'
 with open('test_accounts.json') as accounts:
     ACCOUNTS = json.load(accounts)
 
-ACCOUNTS_ID = list()
+VALID_ACCOUNTS = ACCOUNTS['valid_accounts']
 
 INVALID_ID = '000000'
 
 
 class TestApi(unittest.TestCase):
-    def test1_api_is_running(self):
+    def test_api_ping(self):
         response = requests.get(API).json()
 
         self.assertEqual(response['message'], 'Server is running')
 
-    def test2_create_account(self):
-        # Create valid accounts
-        valid_accounts = ACCOUNTS['valid_accounts']
+    def test_create_account(self):
+        accounts_id = list()
 
-        for account in valid_accounts:
+        # Create valid accounts
+        for account in VALID_ACCOUNTS:
             response = requests.post(f'{API}accounts/register', data=account)
 
             self.assertTrue(response.json()['account'])
 
             id = response.json()['account']
-            ACCOUNTS_ID.append(id)
+            accounts_id.append(id)
 
         # Create invalid accounts
         invalid_accounts = ACCOUNTS['invalid_accounts']
@@ -46,15 +46,26 @@ class TestApi(unittest.TestCase):
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json()['error'], 'Email already exists')
 
-    def test3_get_account(self):
+        # Remove the accounts
+        for id in accounts_id:
+            requests.delete(f'{API}accounts/{id}')
+
+    def test_get_account(self):
+        # Create accounts
+        for account in VALID_ACCOUNTS:
+            requests.post(f'{API}accounts/register', data=account)
+
         # Get all accounts
         response = requests.get(f'{API}accounts/')
 
-        self.assertEqual(len(response.json()), 3)
+        accounts = response.json()
+
+        self.assertEqual(len(accounts), 3)
         self.assertEqual(response.status_code, 200)
 
         # Get specific accounts
-        for id in ACCOUNTS_ID:
+        for account in accounts:
+            id = account['_id']
             response = requests.get(f'{API}accounts/{id}')
 
             self.assertTrue(response.json())
@@ -65,9 +76,18 @@ class TestApi(unittest.TestCase):
 
         self.assertEqual(response.status_code, 204)
 
-    def test4_edit_account(self):
-        # Edit first account
-        id = ACCOUNTS_ID[0]
+        # Delete accounts
+        for account in accounts:
+            id = account['_id']
+            response = requests.delete(f'{API}accounts/{id}')
+
+    def test_edit_account(self):
+        # Create account
+        response = requests.post(
+            f'{API}accounts/register', data=VALID_ACCOUNTS[0])
+
+        # Edit account
+        id = response.json()['account']
         response = requests.patch(f'{API}accounts/{id}',
                                   data={"name": "modified name"})
 
@@ -78,9 +98,19 @@ class TestApi(unittest.TestCase):
 
         self.assertEqual(name, "modified name")
 
-    def test5_delete_account(self):
+        # Delete account
+        requests.delete(f'{API}accounts/{id}')
+
+    def test_delete_account(self):
+        # Create accounts
+        for account in VALID_ACCOUNTS:
+            requests.post(f'{API}accounts/register', data=account)
+
+        accounts = requests.get(f'{API}accounts/').json()
+
         # Deletes all account
-        for id in ACCOUNTS_ID:
+        for account in accounts:
+            id = account['_id']
             response = requests.delete(f'{API}accounts/{id}')
 
             self.assertEqual(response.json()['ok'], 1)
