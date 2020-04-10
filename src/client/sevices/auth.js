aefd87800beb57c1f7cbd3f86b6e8aa65dde502d
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2020-04-10 19:19:23 -0300

    Corrige bug de autenticação

--- src/client/sevices/auth.js
+++ src/client/sevices/auth.js
@@ -1,10 +1,23 @@
+import api from "./api";
+
 export const TOKEN_KEY = "@housing-Token";
 
-export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
+export const isAuthenticated = () =>
+  localStorage.getItem(TOKEN_KEY) !== null && verifyAuthentication();
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
-export const login = token => {
+export const login = (token) => {
   localStorage.setItem(TOKEN_KEY, token);
 };
 export const logout = () => {
   localStorage.removeItem(TOKEN_KEY);
+};
+
+const verifyAuthentication = async () => {
+  const res = await api.get(`/api/logged`);
+
+  if (res.data.error) {
+    logout();
+    location.reload(true);
+  }
+  return !res.data.error;
 };
--- src/server/routes/verifyToken.js
+++ src/server/routes/verifyToken.js
@@ -1,14 +1,22 @@
 const jwt = require("jsonwebtoken");
+const Account = require("../models/Account");
 
-module.exports = function(req, res, next) {
+module.exports = function (req, res, next) {
   const authHeader = req.header("Authorization");
   if (!authHeader) return res.status(401).json({ error: "Access denied" });
 
   // authHeader = Bearer {token}
   const token = authHeader.split(" ")[1];
 
-  jwt.verify(token, process.env.TOKEN_SECRET, (err, account) => {
+  jwt.verify(token, process.env.TOKEN_SECRET, async (err, account) => {
+    // If there was an error, return
     if (err) return res.status(403).json({ error: "Invalid token" });
+
+    // If the account doesn't exist, return
+    const acc = await Account.findById(account._id);
+    if (acc === null) return res.json({ error: "Account not found" });
+
+    // Else, add account to request
     req.account = account;
     next();
   });
